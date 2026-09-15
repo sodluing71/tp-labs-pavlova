cc69b32a64d8fec140df132cc6b5b80d8f8774bf
Anastasia Pavlova <nastja170806@gmail.com>
2026-09-15 21:52:48 +0700

    Lab1: проверка примера из раздела 3 (подсчет факториала)

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Lab1. Вычисление факториала
+Console.Write("Введите n (0..20): ");
+
+if (!int.TryParse(Console.ReadLine(), out int n) || n < 0 || n > 20)
+{
+    Console.WriteLine("Ошибка: нужно целое число от 0 до 20.");
+    return;
+}
+
+Console.WriteLine($"{n}! = {Factorial(n)}");
+Console.ReadLine();
+
+static long Factorial(int n)
+{
+    long result = 1;
+    for (int i = 2; i <= n; i++)
+        result *= i;
+    return result;
+}
