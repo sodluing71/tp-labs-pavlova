61b408ee768a40911040f34918171215b7e0d109
Anastasia Pavlova <nastja170806@gmail.com>
2026-09-15 20:51:47 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp_tp_laba1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
